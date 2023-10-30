1bad9de60a7cc38f2cf05967e749195d6075549f
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-30 23:37:50 +0900

    refactor: Computer 클래스 일급 컬렉션 적용

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,36 +1,27 @@
 package baseball.model;
 
+import baseball.constant.Config;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Computer {
-    private static final int MIN_RANDOM_NUMBER = 1;
-    private static final int MAX_RANDOM_NUMBER = 9;
-    private static final int GAME_NUMBER_LENGTH = 3;
-    private List<Integer> numbers;
+    private Numbers numbers;
 
-    public Computer() {}
-
-    public List<Integer> getNumbers() {
-        if(numbers.isEmpty()) {
-            return null;
-        }
-
+    public Numbers getNumbers() {
         return numbers;
     }
 
     public void setRandomNumbers() {
         Set<Integer> computerNumber = new HashSet<>();
 
-        while(computerNumber.size() < GAME_NUMBER_LENGTH) {
-            int randomNumber = Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+        while(computerNumber.size() < Config.GAME_NUMBER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(Config.MIN_RANDOM_NUMBER, Config.MAX_RANDOM_NUMBER);
 
             computerNumber.add(randomNumber);
         }
 
-        this.numbers = computerNumber.stream().toList();
+        this.numbers = new Numbers(computerNumber.stream().toList());
     }
 }
