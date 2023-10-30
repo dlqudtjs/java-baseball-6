87b29e65ccb5f848d6a7ad4612e1fd008e07a160
dlqudtjs <dlqudtjs922@gmail.com>
2023-10-30 23:25:26 +0900

    refactor: 상수 값을 정의한 클래스 생성

---
+++ src/main/java/baseball/constant/Config.java
@@ -0,0 +1,10 @@
+package baseball.constant;
+
+public class Config {
+
+    public static final int MIN_RANDOM_NUMBER = 1;
+    public static final int MAX_RANDOM_NUMBER = 9;
+    public static final int GAME_NUMBER_LENGTH = 3;
+    public static final String GAME_RESTART_STATE = "1";
+    public static final String GAME_END_STATE = "2";
+}
